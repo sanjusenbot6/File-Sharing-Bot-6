6f2e8034100694d9b4e27b7a821f3592fc3fa317
sanjusenbot6 <hubgit666@gmail.com>
2025-03-03 09:37:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002313226111"))
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002187804785"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "300")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "30")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
